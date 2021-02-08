9dac9ee8388c5adb4b3271c752ae483bed15e6d6
pankaixian <1074303389@qq.com>
2021-02-08 17:20:43 +0800

    Commit Message

--- le04-spring-mybatis/src/main/java/com/pan/service/StudentServiceImpl.java
+++ le04-spring-mybatis/src/main/java/com/pan/service/StudentServiceImpl.java
@@ -2,7 +2,11 @@ package com.pan.service;
 
 import com.pan.dao.StudentDAO;
 import com.pan.domain.Student;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class StudentServiceImpl implements StudentService{
@@ -11,7 +15,20 @@ public class StudentServiceImpl implements StudentService{
     public void setStudentDAO(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
     }
-
+    //表示方法需要事务处理
+    @Transactional (
+            //传播行为
+            propagation = Propagation.REQUIRED,//默认值
+            //隔离级别
+            isolation = Isolation.DEFAULT,//默认值
+            //方法内的数据库访问都是只读
+            readOnly = false,//默认值
+            //发生异常回滚
+            rollbackFor = {
+                    NullPointerException.class,
+                    SQLException.class
+            }
+    )
     @Override
     public int addStudent(Student student) {
         return studentDAO.insertStudent();
